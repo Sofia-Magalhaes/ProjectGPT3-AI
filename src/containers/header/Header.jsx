d00af73c070d19504c1710baf3b1794a23b4d941
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2022-12-20 16:46:12 -0300

    Header
    Setando a Header e arrumando algumas partes do navbar

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './header.css';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className=''>Vamos construir algo incrível com GPT-3 Open IA</h1>
+        <p>
+          No entanto, cama qualquer para a indulgência de assistência de viagem desagradável. Nem os pensamentos todos exercem bênçãos.
+          Indulgência maneira tudo alegria alteração turbulenta o apego. Partido que nós anos para pedir permitir pediu.
+        </p>
+        <div className='gpt3__header-content__input'>
+        </div>
+    </div>
   )
 }
 
