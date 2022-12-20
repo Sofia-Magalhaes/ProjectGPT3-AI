92043ffa882556088d808b022222f89f51d56e6b
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2022-12-20 17:50:26 -0300

    Features
    Features Finalizado

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -6,16 +6,16 @@ const WhatGPT3 = () => {
   return (
     <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
       <div className='gpt3__whatgpt3-feature'>
-        <Feature />
+        <Feature title="O que é GPT-3" text="Nós tão opinião amigos me mensagem como prazer. Toda a frente do prato ouviu oh deve. Sua residência defeituosa nem convencida própria. Conexão colocou impossível apartamentos próprios barulhentos. Na união senhoria um insistiu tão humanidade ele. Entrada de solteiro amigável para on by." />
       </div>
       <div className='gpt3__whatgpt3-heading'>
         <h1 className=''>As possibilidades estão além da sua imaginação</h1>
         <p>Explore a Biblioteca</p>
       </div>
       <div className='gpt3__whatgpt3-container'>
-        <Feature />
-        <Feature />
-        <Feature />
+        <Feature title="Chatbots" text="Nós tão opinião amigos me mensagem como prazer. Toda a frente do prato ouviu oh deve."/>
+        <Feature title="Aprendizado" text="Na união senhoria um insistiu tão humanidade ele. Entrada de solteiro amigável para on by. Como colocar impossível apartamentos próprios b"/>
+        <Feature title="Educação" text="Na união senhoria um insistiu tão humanidade ele. Entrada de solteiro amigável para on by. Como colocar impossível apartamentos próprios b"/>
       </div>
     </div>
   )
