4e5b8a7d5f4bda5e8359f017a6caae89c3b929c8
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2022-12-21 16:23:07 -0300

    Possibility
    possibility fanializado

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import possibilityImage from '../../assets/possibility.png'
 import './possibility.css';
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className='gpt3__possibility-image'>
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h4>Solicite acesso antecipado para começar</h4>
+        <h1 className=''>As possibilidades estão além da sua imaginação</h1>
+        <p>No entanto, aceite qualquer indulgência de assistência em viagem desagradável. Nem todos os pensamentos exercem bênçãos. Indulgência forma tudo alegria alteração turbulento o apego. Parte nós anos para pedir permissão.</p>
+        <h4>Solicite acesso antecipado para começar</h4>
+      </div>
+    </div>
   )
 }
 
