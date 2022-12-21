8f0cac465315ed6ba4310d24c569b8eb81c7db7b
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2022-12-21 17:44:13 -0300

    Blog
    Blog finalizado

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -7,7 +7,7 @@ const Blog = () => {
   return (
     <div className='gpt3__blog section__padding' id='blog'>
       <div className='gpt3__blog-heading'>
-        <h1 className='gradient__text'>Muita coisa está acontecendo,Estamos blogando sobre isso.</h1>
+        <h1 className='gradient__text-h1'>Muita coisa está acontecendo,Estamos blogando sobre isso.</h1>
       </div>
       <div className='gpt3__blog-container'>
         <div className='gpt3__blog-container_groupA'>
